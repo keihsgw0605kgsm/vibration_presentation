a793488426f2a35c9d4a85652fef112925043dd0
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-03-31 04:21:15 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // パッケージ群を読み込む
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 // ポート番号を指定する
